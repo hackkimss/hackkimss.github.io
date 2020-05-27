d0bab641402ca428f663c63b5e8e29b872f864b9
hackkimss <hackkimss@gmail.com>
2020-05-27 14:33:58 +0900

    Update chicor.js

--- js/chicor.js
+++ js/chicor.js
@@ -2,7 +2,7 @@ alert('sec test');
 var params = {
             odrSbagNos: ["1"], // �λ컮援щ땲踰덊샇��
             useCupIssuNos: [], // �ъ슜荑좏룿紐⑸줉
-            sbagPrens: [], // �쇳븨諛깆궗���덉젙蹂�
+            sbagPrens: "", // �쇳븨諛깆궗���덉젙蹂�
             goosPrens: [], // �곹뭹�ъ��덉젙蹂�
             fulnm: "abc", // 二쇰Ц�먮챸
             moblNo: "01012341234", // 紐⑤컮�쇰쾲��
